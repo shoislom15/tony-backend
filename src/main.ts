90494211bb815f51cec5ffebe7d64a1ce5d5f8fa
Shoislom Shobaxtomov <shoislomshobaxromov@gmail.com>
2026-02-03 12:40:23 +0500

    env reading

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = app.get(ConfigService).get<number>('PORT', 3000);
+  await app.listen(port);
 }
 bootstrap();
